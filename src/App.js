857613c21b2ff4578fe4b20cd42211ee8bfb0d49
Frank Gao <57463541+FrankQixiangGao@users.noreply.github.com>
2021-03-27 20:05:35 -0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import sidebar from './sidebar'
+
+function App() {
+  return (
+    <div className="app">
+     <h1>Code Buddy</h1>
+     <sidebar/>
+    </div>
+  );
+}
+
+export default App;
